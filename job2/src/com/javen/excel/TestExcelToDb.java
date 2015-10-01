d18ab0aa1885683a183f359230d1f84c9f3f11c2
supeiyong <768039181@qq.com>
2015-10-01 17:58:49 +0800

    整合提交

---
+++ job2/src/com/javen/excel/TestExcelToDb.java
@@ -0,0 +1,36 @@
+package com.javen.excel;
+
+import java.util.List;
+
+import com.javen.db.DBhepler;
+import com.javen.entity.StuEntity;
+import com.javen.service.StuService;
+
+/**
+ * 
+ */
+public class TestExcelToDb {
+    public static void main(String[] args) {
+        //得到表格中所有的数据
+        List<StuEntity> listExcel=StuService.getAllByExcel("d://test.xls");
+        /*//得到数据库表中所有的数据
+        List<StuEntity> listDb=StuService.getAllByDb();*/
+        
+        DBhepler db=new DBhepler();
+        
+        for (StuEntity stuEntity :listExcel) {
+            /*String grade=stuEntity.getGrade();*/
+           /* if (!StuService.isExist(grade)) {*/
+                //不存在就添加
+                String sql="insert into course (grade,majar,majarnum,coursename,coursetype,credit,classhour,labhour,computerhour,date,teacher,notice) values(?,?,?,?,?,?,?,?,?,?,?,?)";
+                String[] str=new String[]{stuEntity.getGrade(),stuEntity.getMajar(),stuEntity.getMajarnum(),stuEntity.getCoursename(),stuEntity.getCoursetype(),stuEntity.getCredit(),stuEntity.getClasshour(),stuEntity.getLabhour(),stuEntity.getComputerhour(),stuEntity.getDate(),stuEntity.getTeacher(),stuEntity.getNotice()};
+                db.AddU(sql, str);
+            /*}else {*/
+                //存在就更新
+                /*String sql="update stu set name=?,sex=?,num=? where id=?";
+                String[] str=new String[]{stuEntity.getName(),stuEntity.getSex(),stuEntity.getNum()+"",id+""};
+                db.AddU(sql, str);*/
+   
+            }
+        }
+    }
